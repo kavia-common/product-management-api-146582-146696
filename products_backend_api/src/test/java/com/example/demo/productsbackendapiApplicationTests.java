b707967beeb46388669ffed95bea586d93707360
Kavia AI <production@kavia.ai>
2025-10-02 14:21:07 +0000

    CheckPoint - cg81fbc53a

---
+++ products_backend_api/src/test/java/com/example/demo/productsbackendapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productsbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class productsbackendapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
